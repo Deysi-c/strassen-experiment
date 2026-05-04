3273c2bc3397888736dc4fce503ed9f02f20d118
DeysiRamos <dramosqui@unsa.edu.pe>
2026-05-04 15:47:00 -0500

    Agrega funciones de suma y resta de matrices

--- src/matrix_utils.cpp
+++ src/matrix_utils.cpp
@@ -12,3 +12,27 @@ Matrix generateMatrix(int n){
 	}
 	return mat;
 }
+
+Matrix addMatrix(const Matrix& A, const Matrix& B) {
+	int n = A.size();
+	Matrix C(n, std::vector<int>(n));
+
+	for(int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			C[i][j] = A[i][j] + B[i][j];
+		}
+	}
+	return C;
+}
+
+Matrix substractMatrix( const Matrix& A, const Matrix& B){
+	int n = A.size();
+	Matrix C(n, std::vector<int>(n));
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			C[i][j] = A[i][j] - B[i][j];
+		}
+	}
+	return C;
+}
