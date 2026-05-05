0c418566191101aa3f0f3a555edcc77edb5f9121
DeysiRamos <dramosqui@unsa.edu.pe>
2026-05-04 23:41:40 -0500

    Implementa medicion de tiempos

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,64 @@
 #include <iostream>
+#include <vector>
+#include <chrono>
+#include <cstdlib>
+
+using namespace std;
+using namespace chrono;
+
+using Matrix = vector<vector<int>>;
+
+extern Matrix generateMatrix(int n);
+extern Matrix multiplyStandard(const Matrix&, const Matrix&);
+extern Matrix strassen(const Matrix&, const Matrix&);
+
+double median(vector<double>& v){
+	int n = v.size();
+	if(n%2 == 0)
+		return (v[n/2 - 1] + v[n/2]) / 2;
+	else
+		return v[n/2];
+}
+
+double measureStandard(int n) {
+    Matrix A = generateMatrix(n);
+    Matrix B = generateMatrix(n);
+
+    auto start = high_resolution_clock::now();
+    Matrix C = multiplyStandard(A, B);
+    auto end = high_resolution_clock::now();
+
+    return duration<double, milli>(end - start).count();
+}
+
+double measureStrassen(int n) {
+    Matrix A = generateMatrix(n);
+    Matrix B = generateMatrix(n);
+
+    auto start = high_resolution_clock::now();
+    Matrix C = strassen(A, B);
+    auto end = high_resolution_clock::now();
+
+    return duration<double, milli>(end - start).count();
+}
 
 int main() {
-    std::cout << "Experimento Strassen vs Estándar " << std::endl;
-    std::cout << "Proyecto inicializado correctamente" << std::endl;
-    return 0;
+	srand(time(0));
+	vector<int> sizes = {2, 4, 8, 16, 32, 64, 128};
+
+	for (int n : sizes) {
+        vector<double> t1, t2;
+        int runs = 5;
+
+ 		for (int i = 0; i < runs; i++) {
+            		t1.push_back(measureStandard(n));
+            		t2.push_back(measureStrassen(n));
+        	}
+   
+	cout << n <<" "
+		<< median(t1) << " "
+		<<median(t2)
+		<<endl;
+	}
+	return 0;
 }
